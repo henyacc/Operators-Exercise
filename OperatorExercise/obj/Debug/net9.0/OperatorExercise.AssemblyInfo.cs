4dc3a90263aff4b65f232096785a0517ecfdf80a
GRACE <grace@Mac.lan>
2025-07-26 18:54:11 -0400

    completed class exercise

--- OperatorExercise/obj/Debug/net9.0/OperatorExercise.AssemblyInfo.cs
+++ OperatorExercise/obj/Debug/net9.0/OperatorExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e09b5a8b47782d212b2b76c93b064c752c0939c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4453d8a535ea270854c1592e8e9508af774822a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperatorExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
